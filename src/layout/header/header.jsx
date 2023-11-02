39b853b2435a42168233c380dcb9786dd7285b91
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-11-02 08:36:08 +0500

    start app

---
+++ src/layout/header/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "../../config/global-style";
+export const Header = () =>{
+
+    return(
+        <>
+        
+        </>
+    )
+}
---
+++ src/page/home/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const Home = () =>{
+
+    return(
+        <>
+        <h2>home</h2>
+        </>
+    )
+}
